09bc47d1b7decd1af4e28fb09e2b5210b197f7a5
R3tR0@dell <fsR3tR0@github.com>
2021-01-31 18:57:58 -0500

    1.0

---
+++ fft/include.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_H_
+#define INCLUDE_H_
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef unsigned char uch;
+
+#endif /* INCLUDE_H_ */
+
