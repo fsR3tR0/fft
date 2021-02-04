56a098e3cca67a8602886191a59baeedf9d1d3e3
R3tR0@dell <fsR3tR0@github.com>
2021-02-03 22:37:46 -0500

    1.2.2

--- fft/main_fft.c
+++ fft/main_fft.c
@@ -11,6 +11,7 @@ typedef struct list_komplex{
 	float data_Im;
 }list_komplex;
 
+//list fuggvenyek
 list *beszur_elore(list *l0, u8 mit1, float mit2);
 list *beszur_hatra(list *l0, u8 mit1, float mit2);
 void keres(list *l0, u8 mit1, float mit2);
@@ -19,32 +20,39 @@ void print_list(list *l0);
 void list_free(list *l0);
 void lenght_list(list *l0, u8 *var);
 
+//list komplex fuggvenyek
 list_komplex *komp_beszur_elore(list_komplex *lk, float mit1, float mit2);
 void print_komplex(list_komplex *lk);
 
 int main(int argc, char *argv[])
 {
         list *l = NULL;
-        printf("FFT lesz majd\n");
         u8 i = 1;
         float j = 1.2;
         u8 lght = 0;
+
+	printf("=============\n");
+        printf("FFT lesz majd\n");
+	printf("=============\n\n");
+
 	for(u8 i = 0; i < 10; i++){
 		l = beszur_elore(l,i,j);
 		j += 1.2;
 	}
-//	l = beszur_elore(l,2,3.4);
-//	l = beszur_elore(l,3,0.0);
-/*
-        while(i < 12){
-                l = beszur_elore(l,i,j);
-                i++;
-                j += 1.2;
-        }
-*/
         print_list(l);
         lenght_list(l, &lght);
         printf("Hossz: %d\n", lght);
+
+	j = 56.5;
+	for(u8 i = 100; i > 90; i--){
+		l = beszur_hatra(l, i, j);
+		j += 2.4;
+	}
+        print_list(l);
+        lenght_list(l, &lght);
+        printf("Hossz: %d\n", lght);
+
+	keres(l,200,23.4);
 
         list_free(l);
         return 0;
@@ -57,9 +65,38 @@ void print_komplex(list_komplex *lk);
 
 
 list *beszur_hatra(list *l0, u8 mit1, float mit2){
-	
+	list* uj = (list*)malloc(sizeof(list));
+	uj->data_u8 = mit1;
+	uj->data_f = mit2;
+	uj->next = NULL;
+
+	if(l0 == NULL){
+		return uj;
+	}else{
+		list *p = l0;
+		while(p->next != NULL)
+			p = p->next;
+		p->next = uj;
+		return l0;
+	}
 }
-void keres(list *l0, u8 mit1, float mit2);
+
+void keres(list *l0, u8 mit1, float mit2){
+	list *p = l0;
+
+	while(p != NULL){
+		if(p->data_u8 == mit1){
+			printf("Egesz benne van: %d\n", p->data_u8);
+			p = NULL;
+			break;
+		}else{
+			p = p->next;
+		}
+		if(p == NULL)
+			printf("Nincs benne.\n");
+	}
+}
+
 void list_delete_last(list *l0);
 
 void lenght_list(list *l0, u8 *var){
@@ -83,12 +120,18 @@ list *beszur_elore(list *l0, u8 mit1, float mit2){
 
 void print_list(list *l0){
         list *p = l0;
+	u8 db = 0;
         while(p != NULL){
-          //      printf("{%d:%.2f}",p->data_u8, p->data_f);
-		printf("{%d}",p->data_u8);
+        	 printf("{%d:%.2f} ",p->data_u8, p->data_f);
+		//printf("{%d}",p->data_u8);
                 p=p->next;
-                printf("\n");
-        }
+       		db++;
+		if(db == 5){
+			printf("\n");
+			db = 0;
+		}
+	 }
+	printf("\n");
 }
 
 void list_free(list *l0){
